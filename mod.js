2d8c917e4c333200877f99df68e6a90493b28d18
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2024-05-25 02:17:48 +0900

    wip

--- mod.js
+++ mod.js
@@ -1,7 +1,16 @@
 let buffer_next_address = 0;
 
+/**
+ * 
+ * @param { any } initial 
+ * @param { Function } callback 
+ * @returns { number | BigInt }
+ */
+
 const $ = (initial, callback) => {
+
 	const ADDRESS = buffer_next_address;
+
 	Object.defineProperty($, ADDRESS, {
 		get() {
 			return initial
@@ -12,9 +21,12 @@ const $ = (initial, callback) => {
 				: callback?.({ address: ADDRESS, newValue: value, oldValue: initial })
 			return initial = value;
 		},
-		enumerable: false
-	})
-	return buffer_next_address++;
+	});
+
+	return buffer_next_address = buffer_next_address == Number.MAX_SAFE_INTEGER
+
+		? BigInt(buffer_next_address)
+		: buffer_next_address++
 }
 
 export { $ }
